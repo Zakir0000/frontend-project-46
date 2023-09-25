8e69c2d53531058dd10d9eab096c825fbbe4021c
Zakir Khunkaev <zakirkhunkaev@gmail.com>
2023-09-25 09:34:59 +0300

    add option and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,10 +2,18 @@
 
 import { Option, program } from 'commander';
 
+const command = (name1, name2) => {
+  console.log(`Ahoya@!@!@!, ${name1}! and you to fluffyface ${name2}!!`);
+}
+
 program
+  .action(command)
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .addOption(new Option('-V, --version', 'output usage information'))
+  .option('-f, --format <type>', 'output format')
+  .parse(process.argv);
 
-
-program.parse();
+// program.parse();
