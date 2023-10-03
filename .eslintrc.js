a4807a40d0d07a1eb0078037ee74c4cdd1840081
Zakir Khunkaev <zakirkhunkaev@gmail.com>
2023-10-03 14:45:00 +0300

    -

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // other ESLint configurations
+
+  rules: {
+    // other rules
+
+    // Disable quote-props rule
+    'quote-props': 'off',
+
+    // Disable singlequote rule
+    'singlequote': 'off',
+  },
+};
