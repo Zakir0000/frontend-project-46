6e68bf59202386d4bee9d9543a726b6220380c78
Zakir Khunkaev <zakirkhunkaev@gmail.com>
2023-10-13 15:18:14 +0500

    reduce cognitive complexity

--- src/genDiff.js
+++ src/genDiff.js
@@ -4,38 +4,40 @@ import { readFileSync } from 'fs';
 import path from 'path';
 import { parseJSON, parseYAML } from './parsers.js';
 
+function getFileData(filePath) {
+  const resolvePath = path.resolve(filePath);
+  const fileContent = readFileSync(resolvePath, 'utf8');
+  const fileExtension = path.extname(resolvePath);
+  return fileExtension === '.yml' || fileExtension === '.yaml' ? parseYAML(fileContent) : parseJSON(fileContent);
+}
+
+function generateDifferences(data1, data2) {
+  const keys = new Set([...Object.keys(data1), ...Object.keys(data2)]);
+  const result = [];
+
+  for (const key of [...keys].sort()) {
+    if (data1[key] === undefined) {
+      result.push(`  + ${key}: ${data2[key]}`);
+    } else if (data2[key] === undefined) {
+      result.push(`  - ${key}: ${data1[key]}`);
+    } else if (data1[key] === data2[key]) {
+      result.push(`    ${key}: ${data1[key]}`);
+    } else {
+      result.push(`  - ${key}: ${data1[key]}`);
+
+      result.push(`  + ${key}: ${data2[key]}`);
+    }
+  }
+  return result;
+}
+
 export function genDiff(filePath1, filePath2) {
   try {
-    const resolvePath1 = path.resolve(filePath1);
-    const resolvePath2 = path.resolve(filePath2);
+    const data1 = getFileData(filePath1);
+    const data2 = getFileData(filePath2);
+    const differences = generateDifferences(data1, data2);
 
-    const file1 = readFileSync(resolvePath1, 'utf8');
-    const file2 = readFileSync(resolvePath2, 'utf8');
-
-    const fileExtension1 = path.extname(resolvePath1);
-    const fileExtension2 = path.extname(resolvePath2);
-
-    const data1 = fileExtension1 === '.yml' || fileExtension1 === '.yaml' ? parseYAML(file1) : parseJSON(file1);
-
-    const data2 = fileExtension2 === '.yml' || fileExtension2 === '.yaml' ? parseYAML(file2) : parseJSON(file2);
-
-    const keys = new Set([...Object.keys(data1), ...Object.keys(data2)]);
-    const result = [];
-
-    for (const key of [...keys].sort()) {
-      if (data1[key] === undefined) {
-        result.push(`  + ${key}: ${data2[key]}`);
-      } else if (data2[key] === undefined) {
-        result.push(`  - ${key}: ${data1[key]}`);
-      } else if (data1[key] === data2[key]) {
-        result.push(`    ${key}: ${data1[key]}`);
-      } else {
-        result.push(`  - ${key}: ${data1[key]}`);
-
-        result.push(`  + ${key}: ${data2[key]}`);
-      }
-    }
-    return `{\n${result.join('\n')}\n}`;
+    return `{\n${differences.join('\n')}\n}`;
   } catch (error) {
     return `Error: ${error.message}`;
   }
