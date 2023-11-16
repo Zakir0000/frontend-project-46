48add67705f5456533c5209c01d8442f5224835a
Zakir Khunkaev <zakirkhunkaev@gmail.com>
2023-11-16 18:18:46 +0500

    fix diff str-re

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable import/prefer-default-export */
-import { readFileSync } from "fs";
-import path from "path";
-import _ from "lodash";
-import { parseJSON, parseYAML } from "./parsers.js";
+import { readFileSync } from 'fs';
+import path from 'path';
+import _ from 'lodash';
+import { parseJSON, parseYAML } from './parsers.js';
 
 function getFileData(filePath) {
   const resolvePath = path.resolve(filePath);
-  const fileContent = readFileSync(resolvePath, "utf8");
+  const fileContent = readFileSync(resolvePath, 'utf8');
   const fileExtension = path.extname(resolvePath);
-  return fileExtension === ".yml" || fileExtension === ".yaml"
+  return fileExtension === '.yml' || fileExtension === '.yaml'
     ? parseYAML(fileContent)
     : parseJSON(fileContent);
 }
@@ -21,38 +21,38 @@ function makeDiff(data1, data2) {
   const sortedUnionKeys = _.sortBy(unionKeys);
 
   function isPlainObject(obj) {
-    return obj !== null && typeof obj === "object" && obj !== "array";
+    return obj !== null && typeof obj === 'object' && !Array.isArray(obj);
   }
 
   const diff = sortedUnionKeys.map((key) => {
     if (isPlainObject(data1[key]) && isPlainObject(data2[key])) {
       const children = makeDiff(data1[key], data2[key]);
-      return { key, type: "nested", children };
+      return { key, type: 'nested', children };
     }
     if (!_.has(data1, key)) {
       return {
         key,
-        type: "added",
+        type: 'added',
         value: data2[key],
       };
     }
     if (!_.has(data2, key)) {
       return {
         key,
-        type: "deleted",
+        type: 'deleted',
         value: data1[key],
       };
     }
     if (data1[key] === data2[key]) {
       return {
         key,
-        type: "unchanged",
+        type: 'unchanged',
         value: data1[key],
       };
     }
     return {
       key,
-      type: "changed",
+      type: 'changed',
       value1: data1[key],
       value2: data2[key],
     };
@@ -61,15 +61,14 @@ function makeDiff(data1, data2) {
 }
 
 function stringifyValue(value) {
-  if (typeof value === "object" && value !== null) {
+  if (typeof value === 'object' && value !== null) {
     return JSON.stringify(value, null, 2);
-  } else {
-    return String(value);
   }
+  return String(value);
 }
 
 export function genDiff(filePath1, filePath2) {
   const data1 = getFileData(filePath1);
   const data2 = getFileData(filePath2);
-  console.log(stringifyValue(makeDiff(data1, data2)));
+  return stringifyValue(makeDiff(data1, data2));
 }
