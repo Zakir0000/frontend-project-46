0f7329ac05c77b10f405de176fc6a817a0a52b5c
Zakir Khunkaev <zakirkhunkaev@gmail.com>
2023-11-16 10:32:51 +0500

    fix ast structure

--- src/genDiff.js
+++ src/genDiff.js
@@ -4,7 +4,6 @@ import { readFileSync } from "fs";
 import path from "path";
 import _ from "lodash";
 import { parseJSON, parseYAML } from "./parsers.js";
-import { type } from "os";
 
 function getFileData(filePath) {
   const resolvePath = path.resolve(filePath);
@@ -15,80 +14,25 @@ function getFileData(filePath) {
     : parseJSON(fileContent);
 }
 
-function buildAST(obj, key = null) {
-  if (Array.isArray(obj)) {
-    return {
-      type: "array",
-      key,
-      children: obj.map((item, index) => buildAST(item, index.toString())),
-    };
-  } else if (typeof obj === "object" && obj !== null) {
-    return {
-      type: "object",
-      key,
-      children: Object.keys(obj).map((childKey) =>
-        buildAST(obj[childKey], childKey)
-      ),
-    };
-  } else {
-    return {
-      type: "primitive",
-      key,
-      value: obj,
-    };
-  }
-}
-
-function generateDiff(ast1, ast2) {
-  const diff = [];
-
-  const findNodeByKey = (key, nodes) => nodes.find((node) => node.key === key);
-
-  const compareNodes = (node1, node2) => {
-    // If a node is missing in one of the trees, mark it as added or removed
-    if (!node1) {
-      diff.push({ type: "added", key: node2.key, value: node2.value });
-    } else if (!node2) {
-      diff.push({ type: "removed", key: node1.key, value: node1.value });
-    } else {
-      // If both nodes exist, compare their values and children
-      if (node1.children && node2.children) {
-        node1.children.forEach((child1) => {
-          const child2 = findNodeByKey(child1.key, node2.children);
-          compareNodes(child1, child2);
-        });
-
-        // Identify added children
-        node2.children.forEach((child2) => {
-          const child1 = findNodeByKey(child2.key, node1.children);
-          if (!child1) {
-            diff.push({ type: "added", key: child2.key, value: child2.value });
-          }
-        });
-      } else {
-        // Handle the case where one of the nodes doesn't have chidren property
-        if (node1.children) {
-          // Only node1 has children
-          node1.children.forEach((child1) => {
-            diff.push({
-              type: "removed",
-              key: child1.key,
-              value: child1.value,
-            });
-          });
-        } else if (node2.children) {
-          // Only node2 has children
-          node2.children.forEach((child2) => {
-            diff.push({ type: "added", key: child2.key, value: child2.value });
-          });
-        }
+function buildAST(data) {
+  if (_.isObject(data)) {
+    const keys = Object.keys(data);
+    const children = keys.map((key) => {
+      const type = _.isObject(data[key]) ? "tag-internal" : "leaf";
+      const child = {};
+      if (type === "tag-internal") {
+        child.name = key;
+        child.type = type;
+      } else if (type === "leaf") {
+        child.name = key;
+        child.type = type;
+        child.value = data[key];
       }
-    }
-  };
-
-  compareNodes(ast1, ast2);
-
-  return diff;
+      child.children = buildAST(data[key]);
+      return child;
+    });
+    return children;
+  }
 }
 
 function stringifyValue(value) {
@@ -102,8 +46,5 @@ function stringifyValue(value) {
 export function genDiff(filePath1, filePath2) {
   const data1 = getFileData(filePath1);
   const data2 = getFileData(filePath2);
-
-  const ast1 = buildAST(data1);
-  const ast2 = buildAST(data2);
-  return generateDiff(ast1, ast2);
+  console.log(stringifyValue(buildAST(data1)));
 }
