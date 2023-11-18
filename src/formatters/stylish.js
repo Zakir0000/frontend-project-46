eff3448e5003c37f06e4e4fc6c4a5c5011f5def8
Zakir Khunkaev <zakirkhunkaev@gmail.com>
2023-11-18 18:26:37 +0500

    modifing stylishFormatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,25 +2,40 @@ import _ from "lodash";
 
 const stylishFormat = (diff, replacer = " ", spaceCount = 4) => {
   const iter = (currentValue, depth) => {
-    const indent = " ".repeat(spaceCount * depth);
+    const indent = replacer.repeat(spaceCount * depth);
+
+    function stringifyValue(value, depth = 1) {
+      const indentSize = depth * spaceCount;
+      const indent = replacer.repeat(spaceCount * depth);
+      const bracketIndent = replacer.repeat(indentSize - spaceCount);
+      if (_.isObject(value)) {
+        return `{\n${Object.entries(value)
+          .map(
+            ([key, val]) =>
+              `${indent}  ${key}: ${stringifyValue(val, depth + 1)}`
+          )
+          .join("\n")}\n${bracketIndent}}`;
+      }
+      return value;
+    }
 
     const lines = currentValue.flatMap((node) => {
       if (node.type === "nested") {
         const children = iter(node.children, depth + 1);
-        return [` ${indent} ${node.key}: {`, ...children, `${indent}   }`];
+        return [` ${indent} ${node.key}: {`, ...children, `${indent}}`];
       }
 
       const prefix =
         node.type === "added"
-          ? `+ ${node.key}: ${stringifyValue(node.value)}`
+          ? `+ ${node.key}: ${stringifyValue(node.value, depth + 1)}`
           : node.type === "deleted"
-          ? `- ${node.key}: ${stringifyValue(node.value)}`
+          ? `- ${node.key}: ${stringifyValue(node.value, depth + 1)}`
           : node.type === "unchanged"
-          ? `  ${node.key}: ${stringifyValue(node.value)}`
+          ? `  ${node.key}: ${stringifyValue(node.value, depth + 1)}`
           : node.type === "changed"
           ? [
-              `- ${node.key}: ${stringifyValue(node.value1)}`,
-              `+ ${node.key}: ${stringifyValue(node.value2)}`,
+              `- ${node.key}: ${stringifyValue(node.value1, depth + 1)}`,
+              `+ ${node.key}: ${stringifyValue(node.value2, depth + 1)}`,
             ]
           : "  ";
 
@@ -35,12 +50,4 @@ const stylishFormat = (diff, replacer = " ", spaceCount = 4) => {
   return iter(diff, 1).join("\n");
 };
 
-function stringifyValue(value) {
-  if (_.isObject(value)) {
-    return `{\n${Object.entries(value)
-      .map(([key, val]) => `    ${key}: ${stringifyValue(val)}`)
-      .join("\n")}\n  }`;
-  }
-  return value;
-}
 export default stylishFormat;
