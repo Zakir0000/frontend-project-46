13b0d707e401a53abad3e32b60bef0aa4532b0a6
Zakir Khunkaev <zakirkhunkaev@gmail.com>
2023-11-18 16:29:20 +0500

    modifiy stylishFormat

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,35 +2,45 @@ import _ from "lodash";
 
 const stylishFormat = (diff, replacer = " ", spaceCount = 4) => {
   const iter = (currentValue, depth) => {
-    const indent = "   ".repeat(depth);
-    const lines = currentValue.map((node) => {
+    const indent = " ".repeat(spaceCount * depth);
+
+    const lines = currentValue.flatMap((node) => {
       if (node.type === "nested") {
         const children = iter(node.children, depth + 1);
-        return ` ${indent} ${node.key}: {\n${children}\n${indent}    }`;
-      } else if (node.type === "changed") {
-        return `${indent} - ${node.key}: ${node.value1}\n${indent} + ${node.key}: ${node.value2}${indent}`;
-      } else if (_.isObject(node.value)) {
-        const entries = Object.entries(node.value);
-        const [key, value] = entries;
-        return `  ${indent} ${node.key}: {\n${indent}       ${key[0]}: ${key[1]}\n        }`;
+        return [` ${indent} ${node.key}: {`, ...children, `${indent}   }`];
       }
+
       const prefix =
         node.type === "added"
-          ? "+ "
+          ? `+ ${node.key}: ${stringifyValue(node.value)}`
           : node.type === "deleted"
-          ? "- "
+          ? `- ${node.key}: ${stringifyValue(node.value)}`
           : node.type === "unchanged"
-          ? "  "
+          ? `  ${node.key}: ${stringifyValue(node.value)}`
           : node.type === "changed"
-          ? "  "
+          ? [
+              `- ${node.key}: ${stringifyValue(node.value1)}`,
+              `+ ${node.key}: ${stringifyValue(node.value2)}`,
+            ]
           : "  ";
 
-      return `${indent} ${prefix}${node.key}: ${node.value}`;
+      return Array.isArray(prefix)
+        ? prefix.map((line) => `${indent}${line}`)
+        : `${indent}${prefix}`;
     });
-    return lines.join("\n");
+
+    return lines;
   };
 
-  return iter(diff, 1);
+  return iter(diff, 1).join("\n");
 };
 
+function stringifyValue(value) {
+  if (_.isObject(value)) {
+    return `{\n${Object.entries(value)
+      .map(([key, val]) => `    ${key}: ${stringifyValue(val)}`)
+      .join("\n")}\n  }`;
+  }
+  return value;
+}
 export default stylishFormat;
