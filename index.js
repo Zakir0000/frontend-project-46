d0cc097941c9fa89d591988be42ae4d175527130
Zakir Khunkaev <zakirkhunkaev@gmail.com>
2023-11-19 08:38:39 +0500

    -

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-import { genDiff } from './src/genDiff.js';
+import { readFileSync } from "fs";
+import path from "path";
+import _ from "lodash";
+import { parseJSON, parseYAML } from "./src/parsers.js";
+import { makeDiff } from "./src/genDiff.js";
+import mainDiff from "./src/formatters/index.js";
 
-export default genDiff;
+function getFileData(filePath) {
+  const resolvePath = path.resolve(filePath);
+  const fileContent = readFileSync(resolvePath, "utf8");
+  const fileExtension = path.extname(resolvePath);
+  return fileExtension === ".yml" || fileExtension === ".yaml"
+    ? parseYAML(fileContent)
+    : parseJSON(fileContent);
+}
+
+export function genDiff(filePath1, filePath2, format) {
+  const data1 = getFileData(filePath1);
+  const data2 = getFileData(filePath2);
+  const diff = makeDiff(data1, data2);
+  return mainDiff(diff, format);
+}
