647c6e39c0a6b99f63be8aa980e98c10318ceda9
Zakir Khunkaev <zakirkhunkaev@gmail.com>
2023-11-20 15:04:05 +0500

    fix stylishFormatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,68 +1,56 @@
 import _ from "lodash";
 
 const stylishFormat = (diff, replacer = " ", spaceCount = 4) => {
-  const iter = (currentValue, depth, topLevel = true) => {
+  function stringifyValue(value, depth = 1) {
+    const indentSize = depth * spaceCount;
     const indent = replacer.repeat(spaceCount * depth);
+    const bracketIndent = replacer.repeat(indentSize - spaceCount);
+    if (_.isObject(value)) {
+      return `{\n${Object.entries(value)
+        .map(
+          ([key, val]) => `  ${indent}${key}: ${stringifyValue(val, depth + 1)}`
+        )
+        .join("\n")}\n${bracketIndent}  }`;
+    }
+    return value;
+  }
 
-    function stringifyValue(value, depth = 1) {
-      const indentSize = depth * spaceCount;
-      const indent = replacer.repeat(spaceCount * depth);
-      const bracketIndent = replacer.repeat(indentSize - spaceCount);
-      if (_.isObject(value)) {
-        return `{\n${Object.entries(value)
-          .map(
-            ([key, val]) => `${indent}${key}: ${stringifyValue(val, depth + 1)}`
-          )
-          .join("\n")}\n${bracketIndent}}`;
-      }
-      return value;
+  const iter = (currentValue, depth) => {
+    if (!_.isObject(currentValue)) {
+      return `${currentValue}`;
     }
 
-    const lines = currentValue.flatMap((node) => {
-      const nestedIntend = replacer.repeat(spaceCount * depth);
-      if (node.type === "nested") {
-        if (depth >= 1) {
-          const nestedIntend2 = replacer.repeat((spaceCount + 2) * depth);
-          const children = iter(node.children, depth + 1, false);
-          return topLevel
-            ? [
-                `${nestedIntend2}${node.key}: {`,
-                ...children,
-                `${nestedIntend2}}`,
-              ]
-            : children;
-        }
-        const children = iter(node.children, depth + 1);
-        return [
-          `${nestedIntend}${node.key}: {`,
-          ...children,
-          `${nestedIntend}}`,
-        ];
+    const indentSize = depth * spaceCount;
+    const currentIndent = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(indentSize - spaceCount);
+    const lines = Object.values(currentValue).flatMap((obj) => {
+      if (obj.type === "nested") {
+        const children = iter(obj.children, depth + 1);
+        return `  ${currentIndent}${obj.key}: ${children}`;
       }
 
       const prefix =
-        node.type === "added"
-          ? `+ ${node.key}: ${stringifyValue(node.value, depth + 1)}`
-          : node.type === "deleted"
-          ? `- ${node.key}: ${stringifyValue(node.value, depth + 1)}`
-          : node.type === "unchanged"
-          ? `  ${node.key}: ${stringifyValue(node.value, depth + 1)}`
-          : node.type === "changed"
+        obj.type === "added"
+          ? `+ ${obj.key}: ${stringifyValue(obj.value, depth + 1)}`
+          : obj.type === "deleted"
+          ? `- ${obj.key}: ${stringifyValue(obj.value, depth + 1)}`
+          : obj.type === "unchanged"
+          ? `  ${obj.key}: ${stringifyValue(obj.value, depth + 1)}`
+          : obj.type === "changed"
           ? [
-              `- ${node.key}: ${stringifyValue(node.value1, depth + 1)}`,
-              `+ ${node.key}: ${stringifyValue(node.value2, depth + 1)}`,
+              `- ${obj.key}: ${stringifyValue(obj.value1, depth + 1)}`,
+              `+ ${obj.key}: ${stringifyValue(obj.value2, depth + 1)}`,
             ]
           : "  ";
 
       return Array.isArray(prefix)
-        ? prefix.map((line) => `${indent}${line}`)
-        : `${indent}${prefix}`;
+        ? prefix.map((line) => `${currentIndent}${line}`)
+        : `${currentIndent}${prefix}`;
     });
 
-    return topLevel ? ["{", ...lines, `}`] : lines;
+    return ["{", ...lines, `${bracketIndent}}`].join("\n");
   };
-
-  return iter(diff, 1).join("\n");
+  return iter(diff, 1);
 };
 
 export default stylishFormat;
